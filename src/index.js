1fb0360b74f31d2f346503cdb14340b47fdc7f43
Dragonsofash <agpaoajonathanb@gmail.com>
2023-06-08 20:10:18 -0400

    added more styling.

--- src/index.js
+++ src/index.js
@@ -1,50 +1,50 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import companyLogo from "./images/market-logo.png";
 
-import {
-    Login,
-    MessageForm,
-    PostForm,
-    Posts,
-    Profile
-} from './components';
+import { Login, MessageForm, PostForm, Posts, Profile } from "./components";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <div id = "nav">
-                <Link to = "/">Posts</Link>
-                <Link id = "profile" to = "/profile">Profile</Link>
-                <Link to = "/login">Login</Link>
-            </div>
+  return (
+    <BrowserRouter>
+      <div id="nav">
+        <header>
+          <img src={companyLogo} />
+          <h1>Stranger's Things</h1>
+        </header>
+        <nav>
+          <Link to="/">Posts</Link>
+          <Link id="profile" to="/profile">
+            Profile
+          </Link>
+          <Link to="/login">Login</Link>
+        </nav>
+      </div>
 
-            <div id = "content">
-                <Route path = "/">
-                    <Posts />
-                </Route>
+      <div id="content">
+        <Route path="/">
+          <Posts />
+        </Route>
 
-                <Route path = "/profile">
-                    <Profile />
-                </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
 
-                <Route path = "/login">
-                    <Login />
-                </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
 
-                <Route path = "/post_form">
-                    <PostForm />
-                </Route>
+        <Route path="/post_form">
+          <PostForm />
+        </Route>
 
-                <Route path = "/send_message">
-                    <MessageForm />
-                </Route>
-            </div>
-        </BrowserRouter>
-    )
-}
+        <Route path="/send_message">
+          <MessageForm />
+        </Route>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById("app"));
